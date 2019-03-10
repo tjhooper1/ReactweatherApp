056cfbe5d108bca955a5fe85ea0df2789f898058
Sandra Hooper <sandrahooper@Sandras-MacBook-Pro.local>
2019-03-09 20:34:02 -0500

    Updates

---
+++ precache-manifest.e6ab0f12eead7f391b38c03f5bdea8f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5ad223061477c58d6f3",
+    "url": "/ReactweatherApp/static/js/runtime~main.ac7847f5.js"
+  },
+  {
+    "revision": "36597ade17a57faf749c",
+    "url": "/ReactweatherApp/static/js/main.aa7d823e.chunk.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/ReactweatherApp/static/js/2.b19c2b74.chunk.js"
+  },
+  {
+    "revision": "36597ade17a57faf749c",
+    "url": "/ReactweatherApp/static/css/main.ae531445.chunk.css"
+  },
+  {
+    "revision": "bc059ca69bd9657349dcba698c3fea27",
+    "url": "/ReactweatherApp/index.html"
+  }
+];
