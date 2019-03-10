c830283584cbe711657bf5ab0e797913788a1e88
Sandra Hooper <sandrahooper@Sandras-MacBook-Pro.local>
2019-03-09 21:38:11 -0500

    fixed spelling

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,7 +6,7 @@ export default function Weather (props) {
             <div className="heading">Weather app</div>
             <div className="weather-container">
                 <div className="weather-title">Weather for {props.city}</div>    
-                <div className="weather-temperature">{props.temp} degrees farenheit</div>    
+                <div className="weather-temperature">{props.temp} degrees Fahrenheit</div>    
                 <div className="weather-icon"><img src={props.icon} alt="icon" /></div>
                 <form className="form" onSubmit={props.handleClick}>
                     <input onChange={props.handleChange} placeholder="Change city" />
